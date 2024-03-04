ff12816ac78cb3953ff1dfb97ccbe8459432787b
fanil <wolffanil@mail.ru>
2024-03-04 19:02:23 +0400

    first commit

---
+++ lib/canvas.ts
@@ -0,0 +1,346 @@
+import { fabric } from "fabric";
+import { v4 as uuid4 } from "uuid";
+
+import {
+  CanvasMouseDown,
+  CanvasMouseMove,
+  CanvasMouseUp,
+  CanvasObjectModified,
+  CanvasObjectScaling,
+  CanvasPathCreated,
+  CanvasSelectionCreated,
+  RenderCanvas,
+} from "@/types/type";
+import { defaultNavElement } from "@/constants";
+import { createSpecificShape } from "./shapes";
+
+export const initializeFabric = ({
+  fabricRef,
+  canvasRef,
+}: {
+  fabricRef: React.MutableRefObject<fabric.Canvas | null>;
+  canvasRef: React.MutableRefObject<HTMLCanvasElement | null>;
+}) => {
+  const canvasElement = document.getElementById("canvas");
+
+  const canvas = new fabric.Canvas(canvasRef.current, {
+    width: canvasElement?.clientWidth,
+    height: canvasElement?.clientHeight,
+  });
+
+  fabricRef.current = canvas;
+
+  return canvas;
+};
+
+export const handleCanvasMouseDown = ({
+  options,
+  canvas,
+  selectedShapeRef,
+  isDrawing,
+  shapeRef,
+}: CanvasMouseDown) => {
+  const pointer = canvas.getPointer(options.e);
+
+
+  const target = canvas.findTarget(options.e, false);
+
+  canvas.isDrawingMode = false;
+
+  if (selectedShapeRef.current === "freeform") {
+    isDrawing.current = true;
+    canvas.isDrawingMode = true;
+    canvas.freeDrawingBrush.width = 5;
+    return;
+  }
+
+  canvas.isDrawingMode = false;
+
+  if (
+    target &&
+    (target.type === selectedShapeRef.current ||
+      target.type === "activeSelection")
+  ) {
+    isDrawing.current = false;
+
+    canvas.setActiveObject(target);
+
+  
+    target.setCoords();
+  } else {
+    isDrawing.current = true;
+
+    shapeRef.current = createSpecificShape(
+      selectedShapeRef.current,
+      pointer as any
+    );
+
+    if (shapeRef.current) {
+      canvas.add(shapeRef.current);
+    }
+  }
+};
+
+export const handleCanvaseMouseMove = ({
+  options,
+  canvas,
+  isDrawing,
+  selectedShapeRef,
+  shapeRef,
+  syncShapeInStorage,
+}: CanvasMouseMove) => {
+  if (!isDrawing.current) return;
+  if (selectedShapeRef.current === "freeform") return;
+
+  canvas.isDrawingMode = false;
+
+  const pointer = canvas.getPointer(options.e);
+
+
+  switch (selectedShapeRef?.current) {
+    case "rectangle":
+      shapeRef.current?.set({
+        width: pointer.x - (shapeRef.current?.left || 0),
+        height: pointer.y - (shapeRef.current?.top || 0),
+      });
+      break;
+
+    case "circle":
+      shapeRef.current.set({
+        radius: Math.abs(pointer.x - (shapeRef.current?.left || 0)) / 2,
+      });
+      break;
+
+    case "triangle":
+      shapeRef.current?.set({
+        width: pointer.x - (shapeRef.current?.left || 0),
+        height: pointer.y - (shapeRef.current?.top || 0),
+      });
+      break;
+
+    case "line":
+      shapeRef.current?.set({
+        x2: pointer.x,
+        y2: pointer.y,
+      });
+      break;
+
+    case "image":
+      shapeRef.current?.set({
+        width: pointer.x - (shapeRef.current?.left || 0),
+        height: pointer.y - (shapeRef.current?.top || 0),
+      });
+
+    default:
+      break;
+  }
+
+ 
+  canvas.renderAll();
+
+  if (shapeRef.current?.objectId) {
+    syncShapeInStorage(shapeRef.current);
+  }
+};
+
+export const handleCanvasMouseUp = ({
+  canvas,
+  isDrawing,
+  shapeRef,
+  activeObjectRef,
+  selectedShapeRef,
+  syncShapeInStorage,
+  setActiveElement,
+}: CanvasMouseUp) => {
+  isDrawing.current = false;
+  if (selectedShapeRef.current === "freeform") return;
+
+  syncShapeInStorage(shapeRef.current);
+
+  shapeRef.current = null;
+  activeObjectRef.current = null;
+  selectedShapeRef.current = null;
+
+  if (!canvas.isDrawingMode) {
+    setTimeout(() => {
+      setActiveElement(defaultNavElement);
+    }, 700);
+  }
+};
+
+export const handleCanvasObjectModified = ({
+  options,
+  syncShapeInStorage,
+}: CanvasObjectModified) => {
+  const target = options.target;
+  if (!target) return;
+
+  if (target?.type == "activeSelection") {
+  } else {
+    syncShapeInStorage(target);
+  }
+};
+
+export const handlePathCreated = ({
+  options,
+  syncShapeInStorage,
+}: CanvasPathCreated) => {
+  const path = options.path;
+  if (!path) return;
+
+  path.set({
+    objectId: uuid4(),
+  });
+
+  syncShapeInStorage(path);
+};
+
+export const handleCanvasObjectMoving = ({
+  options,
+}: {
+  options: fabric.IEvent;
+}) => {
+  const target = options.target as fabric.Object;
+
+  const canvas = target.canvas as fabric.Canvas;
+
+  target.setCoords();
+
+  if (target && target.left) {
+    target.left = Math.max(
+      0,
+      Math.min(
+        target.left,
+        (canvas.width || 0) - (target.getScaledWidth() || target.width || 0)
+      )
+    );
+  }
+
+  if (target && target.top) {
+    target.top = Math.max(
+      0,
+      Math.min(
+        target.top,
+        (canvas.height || 0) - (target.getScaledHeight() || target.height || 0)
+      )
+    );
+  }
+};
+
+export const handleCanvasSelectionCreated = ({
+  options,
+  isEditingRef,
+  setElementAttributes,
+}: CanvasSelectionCreated) => {
+  if (isEditingRef.current) return;
+
+  if (!options?.selected) return;
+
+  const selectedElement = options?.selected[0] as fabric.Object;
+
+  if (selectedElement && options.selected.length === 1) {
+    const scaledWidth = selectedElement?.scaleX
+      ? selectedElement?.width! * selectedElement?.scaleX
+      : selectedElement?.width;
+
+    const scaledHeight = selectedElement?.scaleY
+      ? selectedElement?.height! * selectedElement?.scaleY
+      : selectedElement?.height;
+
+    setElementAttributes({
+      width: scaledWidth?.toFixed(0).toString() || "",
+      height: scaledHeight?.toFixed(0).toString() || "",
+      fill: selectedElement?.fill?.toString() || "",
+      stroke: selectedElement?.stroke || "",
+      // @ts-ignore
+      fontSize: selectedElement?.fontSize || "",
+      // @ts-ignore
+      fontFamily: selectedElement?.fontFamily || "",
+      // @ts-ignore
+      fontWeight: selectedElement?.fontWeight || "",
+    });
+  }
+};
+
+export const handleCanvasObjectScaling = ({
+  options,
+  setElementAttributes,
+}: CanvasObjectScaling) => {
+  const selectedElement = options.target;
+
+  const scaledWidth = selectedElement?.scaleX
+    ? selectedElement?.width! * selectedElement?.scaleX
+    : selectedElement?.width;
+
+  const scaledHeight = selectedElement?.scaleY
+    ? selectedElement?.height! * selectedElement?.scaleY
+    : selectedElement?.height;
+
+  setElementAttributes((prev) => ({
+    ...prev,
+    width: scaledWidth?.toFixed(0).toString() || "",
+    height: scaledHeight?.toFixed(0).toString() || "",
+  }));
+};
+
+export const renderCanvas = ({
+  fabricRef,
+  canvasObjects,
+  activeObjectRef,
+}: RenderCanvas) => {
+  fabricRef.current?.clear();
+
+  Array.from(canvasObjects, ([objectId, objectData]) => {
+  
+    fabric.util.enlivenObjects(
+      [objectData],
+      (enlivenedObjects: fabric.Object[]) => {
+        enlivenedObjects.forEach((enlivenedObj) => {
+          if (activeObjectRef.current?.objectId === objectId) {
+            fabricRef.current?.setActiveObject(enlivenedObj);
+          }
+
+          fabricRef.current?.add(enlivenedObj);
+        });
+      },
+    
+      "fabric"
+    );
+  });
+
+  fabricRef.current?.renderAll();
+};
+
+export const handleResize = ({ canvas }: { canvas: fabric.Canvas | null }) => {
+  const canvasElement = document.getElementById("canvas");
+  if (!canvasElement) return;
+
+  if (!canvas) return;
+
+  canvas.setDimensions({
+    width: canvasElement.clientWidth,
+    height: canvasElement.clientHeight,
+  });
+};
+
+export const handleCanvasZoom = ({
+  options,
+  canvas,
+}: {
+  options: fabric.IEvent & { e: WheelEvent };
+  canvas: fabric.Canvas;
+}) => {
+  const delta = options.e?.deltaY;
+  let zoom = canvas.getZoom();
+
+  const minZoom = 0.2;
+  const maxZoom = 1;
+  const zoomStep = 0.001;
+
+  zoom = Math.min(Math.max(minZoom, zoom + delta * zoomStep), maxZoom);
+
+  canvas.zoomToPoint({ x: options.e.offsetX, y: options.e.offsetY }, zoom);
+
+  options.e.preventDefault();
+  options.e.stopPropagation();
+};
---
+++ lib/key-events.ts
@@ -0,0 +1,116 @@
+import { fabric } from "fabric";
+import { v4 as uuidv4 } from "uuid";
+
+import { CustomFabricObject } from "@/types/type";
+
+export const handleCopy = (canvas: fabric.Canvas) => {
+  const activeObjects = canvas.getActiveObjects();
+  if (activeObjects.length > 0) {
+    const serializedObjects = activeObjects.map((obj) => obj.toObject());
+    localStorage.setItem("clipboard", JSON.stringify(serializedObjects));
+  }
+
+  return activeObjects;
+};
+
+export const handlePaste = (
+  canvas: fabric.Canvas,
+  syncShapeInStorage: (shape: fabric.Object) => void
+) => {
+  if (!canvas || !(canvas instanceof fabric.Canvas)) {
+    console.error("Invalid canvas object. Aborting paste operation.");
+    return;
+  }
+
+  const clipboardData = localStorage.getItem("clipboard");
+
+  if (clipboardData) {
+    try {
+      const parsedObjects = JSON.parse(clipboardData);
+      parsedObjects.forEach((objData: fabric.Object) => {
+        fabric.util.enlivenObjects(
+          [objData],
+          (enlivenedObjects: fabric.Object[]) => {
+            enlivenedObjects.forEach((enlivenedObj) => {
+              enlivenedObj.set({
+                left: enlivenedObj.left || 0 + 20,
+                top: enlivenedObj.top || 0 + 20,
+                objectId: uuidv4(),
+                fill: "#aabbcc",
+              } as CustomFabricObject<any>);
+
+              canvas.add(enlivenedObj);
+              syncShapeInStorage(enlivenedObj);
+            });
+            canvas.renderAll();
+          },
+          "fabric"
+        );
+      });
+    } catch (error) {
+      console.error("Error parsing clipboard data:", error);
+    }
+  }
+};
+
+export const handleDelete = (
+  canvas: fabric.Canvas,
+  deleteShapeFromStorage: (id: string) => void
+) => {
+  const activeObjects = canvas.getActiveObjects();
+  if (!activeObjects || activeObjects.length === 0) return;
+
+  if (activeObjects.length > 0) {
+    activeObjects.forEach((obj: CustomFabricObject<any>) => {
+      if (!obj.objectId) return;
+      canvas.remove(obj);
+      deleteShapeFromStorage(obj.objectId);
+    });
+  }
+
+  canvas.discardActiveObject();
+  canvas.requestRenderAll();
+};
+
+export const handleKeyDown = ({
+  e,
+  canvas,
+  undo,
+  redo,
+  syncShapeInStorage,
+  deleteShapeFromStorage,
+}: {
+  e: KeyboardEvent;
+  canvas: fabric.Canvas | any;
+  undo: () => void;
+  redo: () => void;
+  syncShapeInStorage: (shape: fabric.Object) => void;
+  deleteShapeFromStorage: (id: string) => void;
+}) => {
+  if ((e?.ctrlKey || e?.metaKey) && e.keyCode === 67) {
+    handleCopy(canvas);
+  }
+
+  if ((e?.ctrlKey || e?.metaKey) && e.keyCode === 86) {
+    handlePaste(canvas, syncShapeInStorage);
+  }
+
+ 
+
+  if ((e?.ctrlKey || e?.metaKey) && e.keyCode === 88) {
+    handleCopy(canvas);
+    handleDelete(canvas, deleteShapeFromStorage);
+  }
+
+  if ((e?.ctrlKey || e?.metaKey) && e.keyCode === 90) {
+    undo();
+  }
+
+  if ((e?.ctrlKey || e?.metaKey) && e.keyCode === 89) {
+    redo();
+  }
+
+  if (e.keyCode === 191 && !e.shiftKey) {
+    e.preventDefault();
+  }
+};
